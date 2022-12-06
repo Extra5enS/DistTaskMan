1663621d404d0f6001eb7d5d02c0595b93f75320
extra5ens <molotkov.md@phystech.edu>
2022-12-07 00:31:06 +0300

    Add machine state struct

---
+++ machine/machine_state/machine-state.go
@@ -0,0 +1,16 @@
+package machine_state
+
+type MachineState struct {
+	E         error
+	AmIMaster bool
+	Net       NetworkState
+}
+
+type NetworkState struct {
+	MyLvL         int
+	MyNum         uint // generates when master process starts
+	MyAddress     string
+	UpAddress     string
+	DownAddresses []string
+	MasterAddress string
+}
